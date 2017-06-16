e5888f13ad762f90edbd23877df05deff908e79c
jamessraun <jeezybig@gmail.com>
2017-06-16 08:58:25 +0700

    chatting with react firebase

---
+++ client/src/App.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { Header,Icon,Button, Comment, Form, Loader} from 'semantic-ui-react'
+import logo from './logo.svg';
+import './App.css';
+
+
+import * as firebase from 'firebase';
+
+var config = {
+   apiKey: "AIzaSyC0sc-CMwxusNce54bS4o7YDdzEGDRGK4Y",
+   authDomain: "cool-chat-82bb3.firebaseapp.com",
+   databaseURL: "https://cool-chat-82bb3.firebaseio.com",
+   projectId: "cool-chat-82bb3",
+   storageBucket: ""
+ };
+
+firebase.initializeApp(config)
+const rootRef = firebase.database().ref().child('chats')
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {chats:[], new_text: ''};
+  }
+
+
+  componentDidMount(){
+
+    let self = this
+    rootRef.on('value',snap => {
+      self.setState({
+        chats: snap.val()
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+
+        </div>
+
+        <div>
+          <Header as='h2' icon textAlign='center'>
+            <Icon name='users' circular />
+            <Header.Content>
+              Everybody can chat!
+            </Header.Content>
+          </Header>
+        </div>
+
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Comment.Group>
+
+          <ChatList chats={this.state.chats} />
+
+            <Form onSubmit={this.handleSubmit}>
+              <Form.TextArea onChange={this.handleChange} value={this.state.text} placeholder='Send Message...' autoHeight/>
+              <Button content='Send' labelPosition='left' icon='edit' primary />
+            </Form>
+          </Comment.Group>
+        </div>
+
+      </div>
+    );
+  }
+
+
+  handleChange(e) {
+    this.setState({new_text: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    var time = new Date().getTime();
+    var newdate = new Date(time);
+
+    var new_chat = {
+      text: this.state.new_text,
+      date: newdate.toString()
+    };
+    rootRef.push(new_chat)
+  }
+}
+
+class ChatList extends React.Component {
+  render() {
+    console.log('thisss----------',this.props);
+    if(this.props.chats.length===0){
+      return (
+        <div>
+          <Loader active inline />
+        </div>
+      );
+    }
+    else{
+      let list=[]
+      let obj = this.props.chats
+      for (var prop in obj) {
+        list.push(obj[prop])
+      }
+      console.log('list---',list);
+    return (
+        <div>
+          {list.map(chat => (
+            <Comment key={chat.date}>
+              <Comment.Avatar as='a' src='https://d30y9cdsu7xlg0.cloudfront.net/png/213810-200.png'/>
+              <Comment.Content>
+                <Comment.Author>Anonymous</Comment.Author>
+                <Comment.Metadata>
+                  <div>{chat.date}</div>
+                </Comment.Metadata>
+                <Comment.Text>
+                  <p>{chat.text}</p>
+                </Comment.Text>
+              </Comment.Content>
+            </Comment>
+          ))}
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
